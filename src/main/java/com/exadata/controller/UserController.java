e4858c4cbc2e0b8ccdddf4ea69339f6aee20fd39
Manit <manit.manu4@gmail.com>
2024-04-12 21:11:05 +0530

    first

--- src/main/java/com/exadata/controller/UserController.java
+++ src/main/java/com/exadata/controller/UserController.java
@@ -30,7 +30,7 @@ public class UserController {
 	public String getessage() {//This is rest end-point...
 		System.out.println("inside getMessage mapping method...");
 		
-		return "hello class jenkins and updating file2....";
+		return "hello class jenkins and updating file3....";
 	}
 	
 //	@RequestMapping("/getUser")
